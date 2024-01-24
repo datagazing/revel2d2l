9d84bcb97f4808ea1c9207d283ca39e67167ab49
Brendan Strejcek <brendan@datagazing.com>
2024-01-24 16:35:17 -0500

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/datagazing/revel2d2l',
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
 )
